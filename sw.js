89e570db5f4d88bf30aff8d37b24fe0013efcc92
oliver603-juang <oliver603@gmail.com>
2026-05-14 22:11:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var V='v37';
+var V='v38';
 self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(['./'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
