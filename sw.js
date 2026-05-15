d70b50b74361e77a3d7cbd2265ea1becf8b2d80b
oliver603-juang <oliver603@gmail.com>
2026-05-15 14:39:48 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'magic-cfop-v37b';
+const CACHE_NAME = 'magic-cfop-v37c';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
