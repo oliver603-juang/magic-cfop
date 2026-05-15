1216af5d6225b840640922eb260d6e2ce66c28f2
oliver603-juang <oliver603@gmail.com>
2026-05-15 14:11:57 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,11 @@
-const CACHE_NAME = 'magic-cfop-v35';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
+const CACHE_NAME = 'magic-cfop-v37';
+const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(c => c.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
