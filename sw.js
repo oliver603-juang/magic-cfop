296be950e1682c575bd7e297378c8e344f20949a
oliver603-juang <oliver603@gmail.com>
2026-07-25 05:03:56 +0800

    v57: verified+fixed all 57 OLL and 21 PLL algorithms, fix S-slice engine bug

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'magic-cfop-v56';
+const CACHE_NAME = 'magic-cfop-v57';
 const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', e => {e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));});
 self.addEventListener('activate', e => {e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));});
